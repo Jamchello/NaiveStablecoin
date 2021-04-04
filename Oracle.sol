6a8683b95d375c0a1ef093a478a8f4a297f1ab9f
jamie michel <jamie.michel@hotmail.com>
2021-04-04 23:47:23 +0100

    ST and BT tokens implemented using OpenZepellin, somewhat functioning mint, burn, deposit and withdrawal functions. Need to improve maths for pricing of BT and potentially add fees to ST operations which can incentivise more backers

--- Oracle.sol
+++ Oracle.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.0;
+//File containing the Source code for mock Oracle providing ETH/GBP value on Ethereum Blockchain.
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/ownership/Ownable.sol";
 
