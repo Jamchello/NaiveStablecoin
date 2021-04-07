6a16c0838d0c7f34e597bbc1266df4e50c277cc4
jamie michel <jamie.michel@hotmail.com>
2021-04-07 13:03:21 +0100

    Buffer working correctly, helper functions added to simplify code and keep it DRY.

--- BT.sol
+++ BT.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0.0;
+pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
